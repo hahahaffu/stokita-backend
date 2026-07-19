2c5aa259ae77d1a85970ec569f4a958c9b140dc6
Haffu <hafizulf25@gmail.com>
2026-07-19 23:53:26 +0700

    Fix email verification bug

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,21 +1,33 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
-  host: process.env.MAIL_HOST,
-
-  port: process.env.MAIL_PORT,
-
+  host: "smtp-relay.gmail.com",
+  port: 587,
   secure: false,
+  requireTLS: true,
 
   auth: {
-
     user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
 
-    pass: process.env.MAIL_PASS
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 
+  tls: {
+    rejectUnauthorized: false,
+  },
+
+  family: 4,
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.error("VERIFY ERROR:", error);
+  } else {
+    console.log("SMTP SERVER READY");
   }
-
 });
 
 module.exports = transporter;
