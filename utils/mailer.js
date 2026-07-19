ff3f354edf8d2ce5b0e4cd78dbdb85dea3767ea3
Haffu <hafizulf25@gmail.com>
2026-07-20 00:18:57 +0700

    Change SMTP provider to Brevo

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,33 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.gmail.com",
-  port: 587,
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
   secure: false,
-  requireTLS: true,
 
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
-
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
-
-  tls: {
-    rejectUnauthorized: false,
-  },
-
-  family: 4,
-});
-
-transporter.verify(function (error, success) {
-  if (error) {
-    console.error("VERIFY ERROR:", error);
-  } else {
-    console.log("SMTP SERVER READY");
-  }
 });
 
 module.exports = transporter;
